980dfb36cb0bf6790b666bc8d5fcf6c9a498bc68
MercOUSE <mercOUSE@yandex.ru>
2022-07-09 17:34:56 +0300

    alpha

--- parser_alpha0.0.1.py
+++ parser_alpha0.0.1.py
@@ -4,12 +4,13 @@ from datetime import datetime
 from datetime import date
 import datetime
 import re
-import string
+from datetime import timedelta
 print("О чём напомнить Вам?")
 text = str(input())
 notif = text + ' ' 
 datex = re.findall('[0-9][0-9][.,-][0-9][0-9][.,-][0-9][0-9][0-9][0-9]',notif)
 print(datex)
+
 test1 = datex[0]
 test2 = test1.split(".")
 now = str(date.today())
@@ -18,6 +19,7 @@ test3 = test2[::-1]
 print(test3)
 now2 = now1
 print(now2)
+
 day_now = int(now2[2])
 month_now = int(now2[1])
 year_now = int(now2[0])
@@ -28,6 +30,15 @@ year_remind = int(test3[0])
 aa = datetime.date(year_remind, month_remind, day_remind)
 bb = datetime.date(year_now, month_now, day_now)
 difference_day = aa-bb
-print(difference_day) 
-    
-day_var = difference_day * 86400
+localtime = difference_day 
+time_d_float = difference_day.total_seconds()
+print(time_d_float) 
+time.sleep(time_d_float)
+words = text.split()
+fragement = test1
+new_words = []
+for word in words:
+    if fragement not in word:
+        new_words.append(word)
+out = ' '.join(new_words)
+print(out)
